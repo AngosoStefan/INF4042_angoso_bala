d0d141ddb21e8c9347d9ca8b4f70034ec289d439
Stefan ANGOSO <stefan.angoso@gmail.com>
2015-12-08 10:10:59 +0100

    Application tutoriel bieres
    Application vue en cours qui permet de télécharger un tableau de bieres
    au format .json et de l'afficher à l'aide d'une recycler view

---
+++ app/src/main/java/org/esiea/angoso_bala/mdcfever/GetBiersServices.java
@@ -0,0 +1,115 @@
+package org.esiea.angoso_bala.mdcfever;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.content.Context;
+import android.nfc.Tag;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * An {@link IntentService} subclass for handling asynchronous task requests in
+ * a service on a separate handler thread.
+ * <p/>
+ * TODO: Customize class - update intent actions, extra parameters and static
+ * helper methods.
+ */
+public class GetBiersServices extends IntentService {
+    public static final String TAG = "GetBiersServices";
+
+    // IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
+    private static final String ACTION_GET_ALL_BIERS = "org.esiea.angoso_bala.mdcfever.action.bier";
+
+    // TODO: Rename parameters
+
+
+    public GetBiersServices() {
+        super("GetBiersServices");
+    }
+
+    /**
+     * Starts this service to perform action Foo with the given parameters. If
+     * the service is already performing a task this action will be queued.
+     *
+     * @see IntentService
+     */
+
+    public static void startActionBiers(Context context) {
+        Intent intent = new Intent(context, GetBiersServices.class);
+        intent.setAction(ACTION_GET_ALL_BIERS);
+        context.startService(intent);
+
+    }
+
+    /**
+     * Starts this service to perform action Baz with the given parameters. If
+     * the service is already performing a task this action will be queued.
+     *
+     * @see IntentService
+     */
+    // TODO: Customize helper method
+    public static void startActionBaz(Context context) {
+        Intent intent = new Intent(context, GetBiersServices.class);
+        context.startService(intent);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        if (intent != null) {
+            final String action = intent.getAction();
+            if (ACTION_GET_ALL_BIERS.equals(action)) {
+                handleActionBiers();
+            }
+        }
+    }
+
+    private void copyInputStreamToFile(InputStream in, File file) {
+        try {
+            OutputStream out = new FileOutputStream(file);
+            byte[] buf = new byte[1024];
+            int len;
+            while ((len = in.read(buf)) > 0) {
+                out.write(buf, 0, len);
+            }
+            out.close();
+            in.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void handleActionBiers() {
+        Log.d(TAG, "Thread:" + Thread.currentThread().getName());
+        URL url = null;
+        try {
+            url = new URL("http://binouze.fabrigli.fr/bieres.json");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+            if (HttpURLConnection.HTTP_OK == conn.getResponseCode()) {
+                copyInputStreamToFile(conn.getInputStream(), new File(getCacheDir(), "bieres.json"));
+                Log.d(TAG, "DOWNLOAD FINISHED FOR JSON BIERES!!!!!!!");
+                LocalBroadcastManager.getInstance(this).sendBroadcast(new Intent(SecondActivity.BIERS_UPDATE));
+            }
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
--- app/src/main/java/org/esiea/angoso_bala/mdcfever/MainActivity.java
+++ app/src/main/java/org/esiea/angoso_bala/mdcfever/MainActivity.java
@@ -1,23 +1,41 @@
 package org.esiea.angoso_bala.mdcfever;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    final Context context = this;
+    DatePickerDialog dpd;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,27 +44,76 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
+//Association des élements graph à leurs équivalent en Java
+        Button b_toast = (Button) findViewById(R.id.b_toast);
+
+//On peut utiliser l'action click
+        b_toast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CharSequence text = "Toast Test working !";
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+                toast.setGravity(Gravity.TOP | Gravity.LEFT, 0, 0);
+            }
+        });
+
+        Button b_popup = (Button) findViewById(R.id.b_popup);
+        b_popup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Dialog dialog = new Dialog(context);
+                dialog.setTitle("Popup Test");
+
+
+                dialog.show();
+
+            }
+        });
+
+/*dpd = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+    @Override
+    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+        //tv_hw.setText
+    }
+},2015 ,11,26);*/
+
+        Button b_activity2 = (Button) findViewById(R.id.b_activity2);
+        b_activity2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+// Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+// Handle action bar item clicks here. The action bar will
+// automatically handle clicks on the Home/Up button, so long
+// as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
+//noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
